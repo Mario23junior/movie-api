10de4ca7b11342626eabc81a988461ffff233501
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-11-20 02:38:02 -0300

    Criando modelo de dados Movie.

---
+++ src/main/java/com/api/movie/model/Movie.java
@@ -0,0 +1,74 @@
+package com.api.movie.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String imagem;
+	private String descricao;
+	private Boolean favorit;
+	private Date dataLancamento;
+
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Boolean getFavorit() {
+		return favorit;
+	}
+
+	public void setFavorit(Boolean favorit) {
+		this.favorit = favorit;
+	}
+
+	public Date getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(Date dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+}
